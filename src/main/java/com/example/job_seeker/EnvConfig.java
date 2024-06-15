90f2fcbce48286c67e00f37ae7a05830f52ded0b
kavi castelo <85213722+kavicastelo@users.noreply.github.com>
2024-06-15 23:29:20 +0530

    Update EnvConfig.java

--- src/main/java/com/example/job_seeker/EnvConfig.java
+++ src/main/java/com/example/job_seeker/EnvConfig.java
@@ -1,1 +1,13 @@
+package com.example.job_seeker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:application.properties")
+public class EnvConfig {
+
+    @Value("${python.exe.path}")
+    public String PYTHON_EXE_PATH;
+}
